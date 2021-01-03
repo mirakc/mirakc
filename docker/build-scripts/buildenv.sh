4b93130097e00774d1b4d13f3935cf0bcc8d9188
masnagam <masnagam@gmail.com>
2021-01-03 14:25:35 +0900

    fix: linux/arm/v7 images were broken

--- docker/build-scripts/buildenv.sh
+++ docker/build-scripts/buildenv.sh
@@ -13,12 +13,14 @@ apt-get install -y --no-install-recommends $BUILD_DEPS
 rustup target add $RUST_TARGET_TRIPLE
 
 if [ "$USE_MUSL" = yes ]; then
-  # Use a cross-compiler which is based on the same version of musl as alpine:3.12.
+  # Use a cross-compiler working with musl@1.1.24 used in alpine:3.12.
   #
   # * https://musl.cc/
   # * https://pkgs.alpinelinux.org/packages?name=musl&branch=v3.12
+  # * http://git.musl-libc.org/cgit/musl/commit/?h=v1.1.24
   #
-  MUSLCC='9.3.1-20200828'
+  # The newest one is 9.2.1-20191012.
+  MUSLCC='9.2.1-20191012'
   ARCHIVE="https://more.musl.cc/$MUSLCC/x86_64-linux-musl/${GCC_HOST_TRIPLE}-cross.tgz"
 
   apt-get install -y --no-install-recommends ca-certificates curl rsync
