6537d6daeaf2737403501e33c732b959ae73fdb5
masnagam <masnagam@gmail.com>
2021-04-14 13:02:55 +0900

    build(deps): bump mirakc-arib from 0.14.4 to 0.14.5

--- docker/build-scripts/mirakc-arib.sh
+++ docker/build-scripts/mirakc-arib.sh
@@ -7,7 +7,7 @@ TARGETPLATFORM=$3
 
 . $BASEDIR/vars.sh
 
-MIRAKC_ARIB_VERSION='0.14.4'
+MIRAKC_ARIB_VERSION='0.14.5'
 ARCHIVE="https://github.com/mirakc/mirakc-arib/archive/$MIRAKC_ARIB_VERSION.tar.gz"
 
 curl -fsSL $ARCHIVE | tar -xz --strip-components=1
