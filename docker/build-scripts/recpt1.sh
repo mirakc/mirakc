f70ac9ade127917f4806b09b2afee28ce32cea1d
masnagam <masnagam@gmail.com>
2023-06-04 09:28:13 +0900

    refactor: change the script a bit

--- docker/build-scripts/recpt1.sh
+++ docker/build-scripts/recpt1.sh
@@ -7,13 +7,14 @@ TARGETPLATFORM=$3
 
 . $BASEDIR/vars.sh
 
-PATCH=https://gist.githubusercontent.com/masnagam/263985322d1eaa5ef2a6e27d57f297d1/raw/2a935310f4521ef245edf1df89282ce5345233f5/stz2012-recpt1-cr.patch
+TARBALL_URL=https://github.com/stz2012/recpt1/tarball/master
+PATCH_URL=https://gist.githubusercontent.com/masnagam/263985322d1eaa5ef2a6e27d57f297d1/raw/2a935310f4521ef245edf1df89282ce5345233f5/stz2012-recpt1-cr.patch
 
-curl -fsSL https://github.com/stz2012/recpt1/tarball/master | tar -xz --strip-components=1
+curl $TARBALL_URL -fsSL | tar -xz --strip-components=1
 cd recpt1
 ./autogen.sh
 ./configure --prefix=/usr/local --host=$GCC_HOST_TRIPLE  # without `--enable-b25`
-curl -fsSL $PATCH | patch -p1  # remove CR in log messages
+curl $PATCH_URL -fsSL | patch -p1  # remove CR in log messages
 if [ "$DISTRO" = alpine ]; then
   sed -i -e 's/^LDFLAGS  =$/LDFLAGS = -static -no-pie/' Makefile
 fi
