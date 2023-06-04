869fc2e322a5eb943a2da9360366432e830e2be8
masnagam <masnagam@gmail.com>
2023-06-04 09:26:38 +0900

    fix: use epgdatacapbon/recdvb
    http://www13.plala.or.jp/sat is no longer available.

--- docker/build-scripts/recdvb.sh
+++ docker/build-scripts/recdvb.sh
@@ -1,7 +1,5 @@
 set -eu
 
-RECDVB='1.3.2'
-
 BASEDIR=$(cd $(dirname $0); pwd)
 DISTRO=$1
 BUILDPLATFORM=$2
@@ -9,11 +7,14 @@ TARGETPLATFORM=$3
 
 . $BASEDIR/vars.sh
 
-curl -fsSL http://www13.plala.or.jp/sat/recdvb/recdvb-$RECDVB.tgz | tar -xz --strip-component=1
+TARBALL_URL=https://github.com/epgdatacapbon/recdvb/tarball/master
+
+curl $TARBALL_URL -fsSL | tar -xz --strip-component=1
 ./autogen.sh
 ./configure --prefix=/usr/local --host=$GCC_HOST_TRIPLE
 sed -i -e 's/msgbuf/_msgbuf/' recpt1core.h
-sed -i '1i#include <sys/types.h>' recpt1.h
+sed -i '1i#include <sys/types.h>' tsmain.c
+sed -i '1i#include <sys/types.h>' tssplitter_lite.c
 if [ "$DISTRO" = alpine ]; then
   sed -i -e 's/^LDFLAGS  =$/LDFLAGS = -static -no-pie/' Makefile
 fi
