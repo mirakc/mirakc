e09f876844ac1abbbe64d52a1cbd52136397561d
masnagam <masnagam@gmail.com>
2023-12-02 12:32:52 +0900

    refactor: refactoring

--- mirakc-core/src/web/server.rs
+++ mirakc-core/src/web/server.rs
@@ -1,4 +1,3 @@
-use std::convert::Infallible;
 use std::path::PathBuf;
 use std::sync::Arc;
 
@@ -26,76 +25,50 @@ pub(super) async fn serve(config: Arc<Config>, app: Router) -> Result<(), Error>
     for path in config.server.uds_paths() {
         set.spawn(uds(path.to_owned(), app.clone()));
     }
-
     while let Some(_) = set.join_next().await {}
     Ok(())
 }
 
-// UDS support based on //examples/unix-domain-socket in tokio-rs/axum.
+// See //examples/serve-with-hyper in tokio-rs/axum.
+// See //examples/unix-domain-socket in tokio-rs/axum.
+
+macro_rules! serve {
+    ($listener:expr, $app:expr) => {
+        let handle = tokio::spawn(async move {
+            let mut make_service = $app.into_make_service_with_connect_info::<PeerInfo>();
+            loop {
+                let (socket, _remote_addr) = $listener.accept().await.unwrap();
+                let tower_service = make_service.call(&socket).await.unwrap();
+                tokio::spawn(async move {
+                    let socket = TokioIo::new(socket);
+                    let hyper_service =
+                        hyper::service::service_fn(move |request: Request<Incoming>| {
+                            tower_service.clone().call(request)
+                        });
+                    if let Err(err) = server::conn::auto::Builder::new(TokioExecutor::new())
+                        .serve_connection(socket, hyper_service)
+                        .await
+                    {
+                        tracing::debug!(?err, "Failed to serve connection");
+                    }
+                });
+            }
+        });
+        let _ = handle.await;
+    };
+}
 
 async fn http(addr: std::net::SocketAddr, app: Router) -> Result<(), Error> {
     let listener = TcpListener::bind(&addr).await?;
-
-    let handle = tokio::spawn(async move {
-        let mut make_service = app.into_make_service_with_connect_info::<PeerInfo>();
-        loop {
-            let (socket, _remote_addr) = listener.accept().await.unwrap();
-            let tower_service = unwrap_infallible(make_service.call(&socket).await);
-            tokio::spawn(async move {
-                let socket = TokioIo::new(socket);
-                let hyper_service =
-                    hyper::service::service_fn(move |request: Request<Incoming>| {
-                        tower_service.clone().call(request)
-                    });
-                if let Err(err) = server::conn::auto::Builder::new(TokioExecutor::new())
-                    .serve_connection_with_upgrades(socket, hyper_service)
-                    .await
-                {
-                    eprintln!("failed to serve connection: {err:#}");
-                }
-            });
-        }
-    });
-
-    let _ = handle.await;
+    serve!(listener, app);
     Ok(())
 }
 
 async fn uds(path: PathBuf, app: Router) -> Result<(), Error> {
-    // Cleanup the previous socket if it exists.
+    // Remove the socket if it exists.
     let _ = tokio::fs::remove_file(&path).await;
     tokio::fs::create_dir_all(path.parent().unwrap()).await?;
-
     let listener = UnixListener::bind(path)?;
-
-    let handle = tokio::spawn(async move {
-        let mut make_service = app.into_make_service_with_connect_info::<PeerInfo>();
-        loop {
-            let (socket, _remote_addr) = listener.accept().await.unwrap();
-            let tower_service = unwrap_infallible(make_service.call(&socket).await);
-            tokio::spawn(async move {
-                let socket = TokioIo::new(socket);
-                let hyper_service =
-                    hyper::service::service_fn(move |request: Request<Incoming>| {
-                        tower_service.clone().call(request)
-                    });
-                if let Err(err) = server::conn::auto::Builder::new(TokioExecutor::new())
-                    .serve_connection_with_upgrades(socket, hyper_service)
-                    .await
-                {
-                    eprintln!("failed to serve connection: {err:#}");
-                }
-            });
-        }
-    });
-
-    let _ = handle.await;
+    serve!(listener, app);
     Ok(())
-}
-
-fn unwrap_infallible<T>(result: Result<T, Infallible>) -> T {
-    match result {
-        Ok(value) => value,
-        Err(err) => match err {},
-    }
 }
