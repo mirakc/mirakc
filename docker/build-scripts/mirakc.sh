e6e373682a6c082b0d4b73bef2f18cd8c7cc7a5c
masnagam <masnagam@gmail.com>
2024-05-09 15:01:13 +0900

    build: remove workaround changes that are no longer needed

--- docker/build-scripts/mirakc.sh
+++ docker/build-scripts/mirakc.sh
@@ -13,22 +13,10 @@ TRIPLE=$(echo "$RUST_TARGET_TRIPLE" | tr '-' '_' | tr [:lower:] [:upper:])
 # Enforce to use a specific compiler in the cc crate.
 export CC_${TRIPLE}="$GCC"
 
-# A workaround to fix the following issue:
-# https://github.com/rust-lang/backtrace-rs/issues/249
-if [ "$TARGETPLATFORM" = linux/arm/v7 ]; then
-  export CFLAGS_${TRIPLE}='-mfpu=neon'
-fi
-
 # Use environment variables instead of creating .cargo/config:
 # https://doc.rust-lang.org/cargo/reference/config.html
 # https://github.com/japaric/rust-cross#cross-compiling-with-cargo
 export CARGO_TARGET_${TRIPLE}_LINKER="$GCC"
-
-# A workaround to fix the following issue:
-# https://github.com/rust-lang/compiler-builtins/issues/201
-if [ "$TARGETPLATFORM" = linux/arm64/v8 ] || [ "$TARGETPLATFORM" = linux/arm64 ]; then
-  export CARGO_TARGET_${TRIPLE}_RUSTFLAGS='-C link-arg=-lgcc'
-fi
 
 export PKG_CONFIG_ALLOW_CROSS=1
 
