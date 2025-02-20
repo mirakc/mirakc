7a0b2fc0b49787bc2c3df6a114ed77b7310c33a6
masnagam <masnagam@gmail.com>
2025-02-21 08:39:08 +0900

    fix: match ergonomics reservations
    https://doc.rust-lang.org/edition-guide/rust-2024/match-ergonomics.html

--- mirakc-core/src/onair/manager.rs
+++ mirakc-core/src/onair/manager.rs
@@ -382,8 +382,8 @@ where
 impl OnairProgramTrackerConfig {
     fn matches(&self, service: &EpgService) -> bool {
         match self {
-            Self::Local(ref config) => config.matches(service),
-            Self::Remote(ref config) => config.matches(service.id),
+            Self::Local(config) => config.matches(service),
+            Self::Remote(config) => config.matches(service.id),
         }
     }
 }
