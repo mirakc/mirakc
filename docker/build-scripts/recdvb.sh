d31e9148a9d7abfcc47c38bcd92fe839fa244818
masnagam <masnagam@gmail.com>
2025-08-30 17:09:11 +0900

    build(deps): update debian base image from bookworm to trixie (#2524)
    * build: switch to `trixie`
    
    * build(deps): update debian base image from bookworm to trixie
    
    (cherry picked from commit 4377ce3bd08bcefffe1e19f5249c0f77bb008b60)

--- docker/build-scripts/recdvb.sh
+++ docker/build-scripts/recdvb.sh
@@ -1,9 +1,8 @@
 set -eu
 
 BASEDIR=$(cd $(dirname $0); pwd)
-DISTRO=$1
-BUILDPLATFORM=$2
-TARGETPLATFORM=$3
+BUILDPLATFORM=$1
+TARGETPLATFORM=$2
 
 . $BASEDIR/vars.sh
 
@@ -15,9 +14,6 @@ curl $TARBALL_URL -fsSL | tar -xz --strip-component=1
 sed -i -e 's/msgbuf/_msgbuf/' recpt1core.h
 sed -i '1i#include <sys/types.h>' tsmain.c
 sed -i '1i#include <sys/types.h>' tssplitter_lite.c
-if [ "$DISTRO" = alpine ]; then
-  sed -i -e 's/^LDFLAGS  =$/LDFLAGS = -static -no-pie/' Makefile
-fi
 make -j $(nproc)
 make install
 $STRIP /usr/local/bin/recdvb
